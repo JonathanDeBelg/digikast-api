8d67506fc692a4ad8b86b16e4c6d53e94b67d9c7
Jonathan Vandionant <jonathan@MacBook-Pro.local>
2022-03-15 11:35:57 +0100

    WIP

--- src/controllers/clothing.controller.js
+++ src/controllers/clothing.controller.js
@@ -29,7 +29,7 @@ const getGarmentSet = catchAsync(async (req, res) => {
 });
 
 const getComperableGarments = catchAsync(async (req, res) => {
-  const garments = await clothingService.getComparableItemsByGarmentId(req.params.garmentId);
+  const garments = await clothingService.getComparableItemsByGarmentId(req.params.garment);
   res.send(garments);
 });
 
