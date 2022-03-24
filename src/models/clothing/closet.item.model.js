f92351167d82b96ad78b236c8965194d3174e85c
Jonathan Vandionant <jonathan@MacBook-Pro.local>
2022-03-24 12:50:20 +0100

    WIP

--- src/models/clothing/closet.item.model.js
+++ src/models/clothing/closet.item.model.js
@@ -54,6 +54,6 @@ closetItemSchema.plugin(toJSON);
 /**
  * @typedef ClosetItem
  */
-const ClosetItem = mongoose.model('Closet-items', closetItemSchema);
+const ClosetItem = mongoose.model('Closet_items', closetItemSchema);
 
 module.exports = ClosetItem;
